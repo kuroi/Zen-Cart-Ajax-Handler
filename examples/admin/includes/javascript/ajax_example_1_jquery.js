d5e12ed369958b3174d2c7b0ee732f3db2507d73
kuroi <peter@kuroiweb.com>
2011-11-26 23:29:28 +0000

    Rename old references sandbox.

--- examples/admin/includes/javascript/ajax_example_1_jquery.js
+++ examples/admin/includes/javascript/ajax_example_1_jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#rand-product a').click(function()
   {
-    $.post('ajax.php', 'handler=ajax_sandbox',function(response){
+    $.post('ajax.php', 'handler=ajax_example_1',function(response){
       if (typeof response.error == 'undefined' || response.error == false) {
         $('#rand-product img').attr("src",response.products_image);
         $('#rand-product p').html(response.products_name);
